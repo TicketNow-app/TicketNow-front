b585fea64dfa2ea1dbd6e3a2254eff1892d3b480
Thiago Leão <45953201+ThiagoLeaoDev@users.noreply.github.com>
2023-02-27 21:31:26 -0300

    🔨 ✨ create date picker component

--- src/screens/Register/index.tsx
+++ src/screens/Register/index.tsx
@@ -6,11 +6,14 @@ import { ButtonBox, ButtonSocialRegisterBox, Container, ContainerAlreadyAccount,
 
 import { Button } from '../../components/Form/Button';
 import { Checkbox } from '../../components/Form/Checkbox';
+import { DatePicker } from '../../components/Form/DatePicker';
 import { Input } from '../../components/Form/Input';
 import { TitleDesc } from '../../components/Form/TitleDesc';
 
 export function Register() {
   const [isChecked, setChecked] = useState(false);
+  const [date, setDate] = useState<Date | string>('');
+  const [datePickerShow, setDatePickerShow] = useState(false);
 
   return (
     <Container>
@@ -20,7 +23,7 @@ export function Register() {
       <Form>
         <TitleDesc title='Crie sua conta' desc='Inscreva-se agora para descobrir os eventos mais incríveis!' />
         <InputBox>
-          <Input placeholder='Data de nascimento' />
+          <Input placeholder='Data de nascimento' editable={false} onPressIn={() => { setDatePickerShow(true) }} value={date?.toLocaleString('pt-BR').split(' ')[0]} />
         </InputBox>
         <InputBox>
           <Input placeholder='Endereço de e-mail' />
@@ -61,6 +64,9 @@ export function Register() {
           </ContainerSocialRegister>
         </ContainerAnotherSignUpWays>
       </Form>
+      {datePickerShow && (
+        <DatePicker handleConfirm={(date) => { setDate(date), setDatePickerShow(false) }} />
+      )}
     </Container>
   );
 }
